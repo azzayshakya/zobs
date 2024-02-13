9d63b148f532771890d844447ab486474a0e3526
Ajay Kumar Shakya <ajayshakya7376@gmail.com>
2024-02-13 21:57:19 +0530

    deploying

--- backend/index.js
+++ backend/index.js
@@ -10,10 +10,10 @@ app.use(cors())
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
-
+ 
 
 app.get("/",(req,res)=>{
-    res.send("hey ajju how are you")
+    res.send("hey ajju how are you , this is your zobs website")
 })
 app.use(express.json());
 app.use(require("./Routes/PostJob"));
